dbe80743d48aad58745ce2fd70b51c48817aba77
BENJAMINMUKIZA <benjaminmukiza123@gmail.com>
2023-03-16 16:20:45 -0700

    done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,10 @@
 
 int main(void)
 {
-	printf("libc6-dev-i386 -m32 gcc\n");
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	printf("Size of double: %zu bytes\n", sizeof(doubleType));
+	printf("Size of char: %zu byte\n", sizeof(charType));
 	return (0);
 
 }
