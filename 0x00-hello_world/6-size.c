186d4c2e5802b0c228a4a14281148b1cf76d7226
BENJAMINMUKIZA <benjaminmukiza123@gmail.com>
2023-03-16 18:51:46 -0700

    done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,19 @@
 
 int main(void)
 {
+	#ifdef __x86_64__
+	printf("64-bit version:\n");
+	#else
+	printf("32-bit version:\n");
+	#endif
+
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
+
 	return (0);
 
 }
