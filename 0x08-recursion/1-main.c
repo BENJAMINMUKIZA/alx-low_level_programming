3a19213f5fa08b8c6cc584455787519c4772c479
BENJAMINMUKIZA <benjaminmukiza123@gmail.com>
2023-04-05 12:20:58 -0700

    recursion

--- 0x08-recursion/1-main.c
+++ 0x08-recursion/1-main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - check the code
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	_print_rev_recursion("\ncolton walker");
+	_print_rev_recursion("\nColton Walker");
 	return (0);
 }
