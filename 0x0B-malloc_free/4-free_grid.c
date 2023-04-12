5bc7b202786154ec5f6f440f5b759c4df5702ddf
BENJAMINMUKIZA <benjaminmukiza123@gmail.com>
2023-04-12 16:41:54 -0700

    done

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * free_grid - function that frees two dimension
+ * @grid: apointer to the grid to be freezed
+ * @height: the height of grid
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
