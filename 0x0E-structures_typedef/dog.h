523a3370950c98c5660a1ee8d1f5f73a6fcb5af8
BENJAMINMUKIZA <benjaminmukiza123@gmail.com>
2023-04-17 19:12:07 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+/**
+ * struct dog - A structure representing a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
